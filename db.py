bea09cd3f703b21336e1d5fba2e34f3f9e303887
afiracp <afiracp@gmail.com>
2026-06-12 11:24:03 +0700

    fix railway deployment

--- db.py
+++ db.py
@@ -1,12 +1,14 @@
 import mysql.connector
+import os
 
 def get_connection():
 
     conn = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="rekomendasi_buku"
+        host=os.getenv("MYSQLHOST"),
+        port=int(os.getenv("MYSQLPORT")),
+        user=os.getenv("MYSQLUSER"),
+        password=os.getenv("MYSQLPASSWORD"),
+        database=os.getenv("MYSQLDATABASE")
     )
 
     return conn
